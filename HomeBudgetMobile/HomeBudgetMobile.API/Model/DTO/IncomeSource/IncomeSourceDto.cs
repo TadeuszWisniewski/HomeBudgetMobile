8598b59bd72d5b866dded3abeff07c646fe1b84b
Tadeusz Lukasz Wisniewski <Wisniewskibe@gmail.com>
2025-01-31 16:01:03 +0100

    Create IncomeSource controller
    Close #12

---
+++ HomeBudgetMobile/HomeBudgetMobile.API/Model/DTO/IncomeSource/IncomeSourceDto.cs
@@ -0,0 +1,9 @@
+﻿namespace HomeBudgetMobile.API.Model.DTO.IncomeSource
+{
+    public class IncomeSourceDto
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string? Description { get; set; }
+    }
+}
---
+++ HomeBudgetMobile/HomeBudgetMobile.API/Repositories/Interfaces/IIncomeSourceRepository.cs
@@ -0,0 +1,13 @@
+﻿using HomeBudgetMobile.API.Model.Domain;
+
+namespace HomeBudgetMobile.API.Repositories.Interfaces
+{
+    public interface IIncomeSourceRepository
+    {
+        Task<List<IncomeSource>> GetAllAsync();
+        Task<IncomeSource?> GetByIdAsync(Guid id);
+        Task<IncomeSource> CreateAsync(IncomeSource incomeSource);
+        Task<IncomeSource?> UpdateAsync(Guid id, IncomeSource incomeSource);
+        Task<IncomeSource?> DeleteAsync(Guid id);
+    }
+}
